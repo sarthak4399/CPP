6d9da2682d927fd22f710801372687966e50d3f3
Sarthak Khandare <sarthakkhandare@Sarthaks-MacBook-Air.local>
2023-07-16 20:30:02 +0530

    🚀

--- HRprob11.cpp
+++ HRprob11.cpp
@@ -1,16 +1,79 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <sstream>
 using namespace std;
-int main () {
 
-    string a ;
-    string b ;
-    cin >> a >> b ;
-    cout << a.size() << " " << b.size() << endl ;
-    cout << a + b << endl ;
-    char temp = a[0] ;
-    a[0] = b[0] ;
-    b[0] = temp ;
-    cout << a << " " << b << endl ;
-    return 0 ;
+
+int main() {
+
+    int N,Q;
+    cin >> N >> Q;
+
+
+    vector<string> prefix_tag;
+
+    unordered_map<string, string> tag_map;
+        
+
+    string line, prefix;
+    
+
+    getline(cin, line);
+    
+    for(int i=0;i<N;i++){
+        getline(cin, line);
+        
+
+        line.erase(line.end()-1);
+        line.erase(line.begin(), line.begin()+1);
+        
+
+        if(line[0]=='/'){
+            line.erase(line.begin(), line.begin()+1);
+            prefix_tag.pop_back();
+            continue;
+        }
+        
+
+        replace(line.begin(), line.end(), '=', ' ');
+        replace(line.begin() , line.end() , '\"' , ' ');
+        
+
+        stringstream ss(line);
+        
+
+        ss >> prefix;
+        prefix_tag.push_back(prefix);
+        
+
+        string key,value, temp;
+        
+
+        for (auto & element : prefix_tag){
+            temp = temp + "." + element;
+        }
+        
+
+        while(ss){
+            ss >> key >> value;
+            tag_map[temp + "~" + key] = value; 
+        }        
+
+    }
+    
+    for(int i=0;i<Q;i++){
+        getline(cin, line);
+        string temp = "."+line;
+        if(tag_map.count(temp)==1){
+            cout << tag_map[temp] << endl;
+        }
+        else{
+            cout << "Not Found!" << endl;
+        }
+    }
+    return 0;
 }
